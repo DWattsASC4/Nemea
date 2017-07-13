a609e8d299683a575420b243dccbf08b6cc6f5f3
NemeaASC4 <AstralNemea@gmail.com>
2017-07-13 05:16:52 -0700

    condtionals and functions

---
+++ condition.js
@@ -0,0 +1,7 @@
+var level = 4;
+
+if (level == 5){
+console.log("you can evolve!");
+}else{
+console.log("you cannot evolve.");
+}      
---
+++ function.js
@@ -0,0 +1,6 @@
+function YorHa(){
+    return "You want to **** 2b don't you?";
+    console.log("You want to **** 2b don't you?");
+}
+
+YorHa();
