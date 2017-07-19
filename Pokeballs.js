46024708c2764e68469971956326ab392191001a
NemeaASC4 <AstralNemea@gmail.com>
2017-07-20 00:55:37 +0900

    Bouncing Ball

--- Pokeballs.js
+++ Pokeballs.js
@@ -1,8 +1,13 @@
 function setup(){
     createCanvas(300,300);
     background(200);
+    
     strokeWeight(5);
     fill(255,0,0);
     ellipse(150,150,200,200);
-    arc(50,150,249,150)
+    ellipse(150,150,50,50)
+    fill(255)
+    arc(150,150,200,200,PI,TWO_PI);
+    
+
 }
